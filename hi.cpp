1a0faf5f57c77879a8e1d3db6c349645733e1fe6
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-04-04 19:04:35 +0200

    Add atomic example

--- hi.cpp
+++ hi.cpp
@@ -1,22 +1,76 @@
-#include <iostream>
-#include <thread>
-#include <chrono>
-
+// include psp headers
 #include <pspkernel.h>
 #include <pspdebug.h>
 #include <pspdisplay.h>
 #include <pspkernel.h>
- 
-PSP_MODULE_INFO("Hello", 0, 1, 1);
-PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER|THREAD_ATTR_VFPU);
 
-int main()
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+ 
+// configure PSP stuff
+#define VERS    1
+#define REVS    0
+ 
+PSP_MODULE_INFO("RetroArch", 0, 1, 1);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER|THREAD_ATTR_VFPU);
+ 
+ 
+// make printing easier on us
+// #define printf pspDebugScreenPrintf
+
+static int exitRequest  = 0;
+
+static int isRunning()
 {
-    using namespace std::chrono_literals;
-    std::cout << "Hello waiter\n" << std::flush;
-    auto start = std::chrono::high_resolution_clock::now();
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> elapsed = end-start;
-    std::cout << "Waited " << elapsed.count() << " ms\n";
+	return !exitRequest;
+}
+
+static int exitCallback(int arg1, int arg2, void *common) 
+{ 
+	exitRequest = 1; 
+	return 0; 
+} 
+
+static int callbackThread(SceSize args, void *argp) 
+{ 
+	int callbackID; 
+
+	callbackID = sceKernelCreateCallback("Exit Callback", exitCallback, NULL); 
+	sceKernelRegisterExitCallback(callbackID); 
+
+	sceKernelSleepThreadCB(); 
+
+	return 0; 
+} 
+
+static int setupExitCallback() 
+{ 
+	int threadID = 0; 
+
+	threadID = sceKernelCreateThread("Callback Update Thread", callbackThread, 0x11, 0xFA0, THREAD_ATTR_USER, 0); 
+	 
+	if(threadID >= 0) 
+	{ 
+		sceKernelStartThread(threadID, 0, 0); 
+	} 
+
+	return threadID; 
+}
+ 
+ 
+int main(int argc, char** argv)
+{
+    // basic init
+    setupExitCallback();
+    
+	while(isRunning()) {
+      printf("Working!!!\n");
+      sceIoWrite(2, "foo\n", 4);
+      sceIoWrite(1, "faa\n", 4);
+   }
+
+   //  exit
+    sceKernelExitGame();
+    return 0;
 }
